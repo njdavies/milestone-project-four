524a3ca21e65caf86c0ed91483791be5769873cf
Nathan Davies <nathandavies06@hotmail.co.uk>
2020-05-20 16:39:30 +0100

    Updated settings.py file for Heroku config

--- ecommerce/settings.py
+++ ecommerce/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/2.2/ref/settings/
 """
 
 import os
+import django_heroku
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -25,7 +26,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = ['https://ecommerce-msp4.herokuapp.com/']
 
 
 # Application definition
@@ -177,3 +178,5 @@ EMAIL_USE_TLS = True
 
 STRIPE_PUBLISHABLE = os.environ.get('STRIPE_PUBLISHABLE')
 STRIPE_SECRET = os.environ.get('STRIPE_SECRET')
+
+django_heroku.settings(locals())
