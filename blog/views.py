14b508d7f67b06b124b0bc160619e5160408ec56
Nathan Davies <nathandavies06@hotmail.co.uk>
2020-05-21 18:08:45 +0100

    Added files

--- blog/views.py
+++ blog/views.py
@@ -30,7 +30,7 @@ class BlogDetailView(DetailView):
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
-        title = "Blog"
+        title = "Blog Post"
         context["title"] = title
         return context
 
