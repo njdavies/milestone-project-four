bba39421da5bdf29553b597dab1e0379a2ad2d4c
Nathan Davies <nathandavies06@hotmail.co.uk>
2020-05-25 15:23:13 +0100

    Fixed account creation form
    The form was not sending a confirmation email upon submission, therefore
    updated code to correct this.

--- accounts/forms.py
+++ accounts/forms.py
@@ -1,4 +1,6 @@
 from django import forms
+from crispy_forms.helper import FormHelper
+from crispy_forms.layout import Submit
 from django.contrib.auth.forms import UserCreationForm, UserChangeForm
 from .models import CustomUser
 
@@ -12,6 +14,11 @@ class CustomUserCreationForm(UserCreationForm):
     class Meta:
         model = CustomUser
         fields = ('username', 'email', 'password1', 'password2')
+
+    def __init__(self, *args, **kwargs):
+        self.helper = FormHelper()
+        self.helper.add_input(Submit('submit', 'Submit'))
+        super(CustomUserCreationForm, self).__init__(*args, **kwargs)
 
 
 class CustomUserChangeForm(UserChangeForm):
