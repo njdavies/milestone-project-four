3e23564ae7510bf6b0510342b7b6629ef6c154e4
Nathan Davies <nathandavies06@hotmail.co.uk>
2020-05-25 11:36:34 +0100

    Updated page titles for Blog app

--- blog/urls.py
+++ blog/urls.py
@@ -4,7 +4,7 @@ from .views import BlogListView, BlogDetailView, add_comment_to_post
 urlpatterns = [
     path('', BlogListView.as_view(), {'title': 'Blog'}, name='blog'),
     path('post/<int:pk>/', BlogDetailView.as_view(),
-         {'title': 'Post'}, name='blog-post'),
-    path('post/<int:pk>/comment/', add_comment_to_post, {'title': 'Comment'},
+         {'title': 'Blog Post'}, name='blog-post'),
+    path('post/<int:pk>/comment/', add_comment_to_post,
          name='add_comment_to_post'),
 ]
