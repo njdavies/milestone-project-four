f27efb25ed5047b6ce8f85f0b05318756dfeb28f
Nathan Davies <nathandavies06@hotmail.co.uk>
2020-06-02 17:33:53 +0100

    Updated contact form email functionality
    Replaced email.js code with django email view to send emails to
    both the user and the site admin when a contact form is submitted.
    
    This change brings consistency to all email functionality across
    the site.

--- pages/urls.py
+++ pages/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
-from .views import HomePageView, AboutPageView, ContactPageView
+from .views import HomePageView, AboutPageView, ContactPageView, contact_form
 
 urlpatterns = [
     path('', HomePageView.as_view(), {'title': 'Home'}, name='home'),
     path('about/', AboutPageView.as_view(), {'title': 'About'}, name='about'),
     path('contact/', ContactPageView.as_view(),
          {'title': 'Contact Us'}, name='contact'),
+    path('contact/form/', contact_form, name='contact_form'),
 ]
